5d65bf102013d66176abc2a5269a67b84c736ffd
Michael Lange <dingoeatingfuzz@gmail.com>
2021-04-03 18:08:07 -0700

    Full colorgit add .

--- play/reporter.js
+++ play/reporter.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk');
+
 class Reporter {
   constructor(runner, ui) {
 
@@ -6,7 +8,7 @@ class Reporter {
     runner.on('runStart', ev => {
       if (ev.testCounts && ev.testCounts.total) {
         const total = ev.testCounts.total;
-        ui.writeLine(`Running ${total} test${total !== 1 ? 's' : ''}...`);
+        ui.writeLine(`Running ${chalk.bold(total)} test${total !== 1 ? 's' : ''}...`);
       } else {
         ui.writeLine('Running tests...');
       }
@@ -15,13 +17,19 @@ class Reporter {
     runner.on('runEnd', ev => {
       ui.writeLine('');
       if (ev.status === 'failed') {
-        ui.writeLine('Run failed.');
+        ui.writeLine(chalk.red.bold('Run failed.'));
       } else if (ev.status === 'passed') {
-        ui.writeLine('Run passed!');
+        ui.writeLine(chalk.green.bold('Run passed!'));
       }
 
-      ui.writeLine(`${ev.testCounts.total} test${ev.testCounts.total !== 1 ? 's' : ''} run in ${humanDuration(ev.runtime)}`);
-      ui.writeLine(`Passes: ${ev.testCounts.passed}, Failures: ${ev.testCounts.failed}, Skipped: ${ev.testCounts.skipped}, Todo: ${ev.testCounts.todo}`);
+      ui.writeLine(`${chalk.bold(ev.testCounts.total)} test${ev.testCounts.total !== 1 ? 's' : ''} run in ${chalk.bold(humanDuration(ev.runtime))}`);
+      const lines = [
+        `Passes: ${ev.testCounts.passed > 0 ? chalk.green.bold(ev.testCounts.passed) : '0' }`,
+        `Failures: ${ev.testCounts.failed > 0 ? chalk.red.bold(ev.testCounts.failed) : '0'}`,
+        `Skipped: ${ev.testCounts.skipped > 0 ? chalk.gray.bold(ev.testCounts.skipped) : '0'}`,
+        `Todo: ${ev.testCounts.todo > 0 ? chalk.yellow.bold(ev.testCounts.todo) : '0'}`
+      ];
+      ui.writeLine(lines.join(', '));
     });
 
     // Suite Events
@@ -42,29 +50,38 @@ class Reporter {
         skipped: ' SKIPPED: ',
         todo: ' TODO: ',
       }[ev.status];
+      const color = {
+        passed: chalk.green,
+        failed: chalk.red,
+        skipped: chalk.gray,
+        todo: chalk.yellow,
+      }[ev.status];
 
       const indent = ev.fullName.length;
+      const line = color(`${space(indent)}${prefix} ${ev.name}`)
+      const timing = chalk.gray(humanDuration(ev.runtime));
+
       if (ev.status === 'failed') {
         // But space around failed tests to make them stand out.
         ui.writeLine('');
       }
-      ui.writeLine(`${space(indent)}${prefix} ${ev.name} [${humanDuration(ev.runtime)}]`);
+      ui.writeLine(`${line} ${timing}`);
 
       // Only list assertions when the test failed
       if (ev.status === 'failed') {
         ev.assertions.forEach(assertion => {
           if (assertion.passed) {
-            ui.writeLine(`${space(indent + 1)}✓ ${assertion.message}`);
+            ui.writeLine(chalk.green(`${space(indent + 1)}✓ ${assertion.message}`));
           } else {
-            ui.writeLine(`${space(indent + 1)}✗ ${assertion.message}`);
+            ui.writeLine(chalk.red(`${space(indent + 1)}✗ ${assertion.message}`));
             ui.writeLine('');
-            ui.writeLine('Expected:');
+            ui.writeLine(chalk.white('Expected:'));
             console.log(assertion.expected);
             ui.writeLine('');
-            ui.writeLine('Actual:');
+            ui.writeLine(chalk.white('Actual:'));
             console.log(assertion.expected);
             ui.writeLine('');
-            ui.writeLine('Stack:');
+            ui.writeLine(chalk.white('Stack:'));
             console.log(assertion.stack);
           }
         });
